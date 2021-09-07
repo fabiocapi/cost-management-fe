7fc86f4b1741daadd3ae40f3a83eb2ec803fa755
Guidus Vendra <guido.vndr@gmail.com>
2021-09-07 22:48:25 +0200

    validazione nell update

--- src/app/create-dipendente/create-dipendente.component.ts
+++ src/app/create-dipendente/create-dipendente.component.ts
@@ -2,12 +2,12 @@ import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { from, Observable, of } from 'rxjs';
 import { Azienda } from '../azienda';
 import { AziendaService } from '../azienda.service';
 import { Dipendente } from '../dipendente';
 import { DipendenteService } from '../dipendente.service';
-import { map, filter, switchMap, take } from 'rxjs/operators';
+import { map, filter, switchMap, take, concatMap, delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-create-dipendente',
@@ -19,40 +19,40 @@ export class CreateDipendenteComponent implements OnInit {
 
   dipendente : Dipendente = new Dipendente();
   selectedeAziendaId: number =1;;
-  defaultAziendaId :number =1;
+  defaultAziendaId :number=1;
   aziende: Observable<Azienda[]>;
-  //aziendaId: number;
-  azienda: Azienda;
+  
 
   constructor(private dipendentiService: DipendenteService,private aziendaService: AziendaService, private router: Router) { 
     this.loadAziende();
-   
   }
+  
 
    async loadAziende() {
     await new Promise(f => setTimeout(f, 50));
-    this.aziende= this.aziendaService.findAllAziende();
-   
-    
-    console.log("AZIENDE ARRAY OBSERVABLE : " +this.aziende);
-    
+    this.aziende = this.aziendaService.findAllAziende();
+    //console.log("AZIENDE ARRAY OBSERVABLE : " +this.aziende);
   }
+
+    /*
+   this.aziendaService.findAllAziende().pipe(
+      switchMap(values => from(values)),
+      concatMap(value => of(value).pipe(delay(500)))
+    );*/
   
   ngOnInit(): void {
+
       }
 
   changeClient(value: any){
     this.selectedeAziendaId = value;
-
    // console.log("AZIENDA SELEZIONATA DAL MENU A TENDINA: "+ this.selectedeAziendaId);
 }
 
   onSubmit(){
-    
     this.dipendentiService.
-    addDipendente(this.dipendente,this.selectedeAziendaId).
-    subscribe(data => {console.log(data), this.goToList()},
-    )
+    addDipendente(this.dipendente,this.selectedeAziendaId).toPromise()
+                              .then(x => this.goToList(), error =>console.log(error));
    }
   goToList(){
     this.router.navigate(['listaDipendenti']);
