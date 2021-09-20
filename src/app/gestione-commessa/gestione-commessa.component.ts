c206679917d21b824631582b54023851f5df44bf
Guidus Vendra <guido.vndr@gmail.com>
2021-09-20 15:09:41 +0200

    ripristino funzioni dipendente dopo il merge e sviluppo iniziale di addCommessa

---
+++ src/app/gestione-commessa/gestione-commessa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gestione-commessa',
+  templateUrl: './gestione-commessa.component.html',
+  styleUrls: ['./gestione-commessa.component.css']
+})
+export class GestioneCommessaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
